177299a5e881ea27be667fcd01401f47bdaab8ab
LabtechConsulting <CTaylor@labtechconsulting.com>
2017-01-11 21:03:46 -0700

    Initial upload

---
+++ Wireless.ps1
@@ -0,0 +1,26 @@
+﻿function Update-WirelessPassword {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$True)]
+        $Profile,
+        $Password
+    )
+
+    $Confirm = Netsh WLAN export profile name="$Profile" folder="$env:temp" key=clear
+    
+    if($($Confirm | Out-String) -notlike '*success*'){
+        Write-Output "ERROR: $Confirm"
+        break
+    }
+    try{
+        $FilePath = (Get-ChildItem $env:temp -Filter "*$($Profile).xml")[0].FullName
+        [xml]$XML = Get-Content $FilePath
+        $XML.WLANProfile.MSM.security.sharedKey.keyMaterial = $Password
+        $XML.Save($FilePath)
+        Netsh WLAN add profile filename="$FilePath"
+        Remove-Item $FilePath -Force
+    }
+    catch{
+        Write-Output "ERROR: $($Error[0])"
+    }
+}
